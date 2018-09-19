e9f467171412660da15a086e4126593e94732d3c
DimitrivanManen <17382@ma-web.nl>
2018-09-19 08:42:02 +0200

    Added Flappy Bird template

---
+++ GPR Infinite Runner 17-9-2018/Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour {
+
+    Rigidbody2D Platform;
+    public float speed = 0.05f;
+
+    private void Awake()
+    {
+        Platform = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        Platform.position = new Vector2(Platform.position.x - speed, Platform.position.y);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Destroyer")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+}
---
+++ GPR Infinite Runner 17-9-2018/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    Rigidbody2D player;
+
+    private void Awake()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            player.AddForce(player.transform.up * 200);
+        }        
+        if(player.position.y < -3.4f)
+        {
+            player.position = new Vector2(player.position.x, -3.4f);
+        }
+        if (player.position.y < -5.5f)
+        {
+            player.position = new Vector2(player.position.x, -5.5f);
+        }
+    }
+
+}
