5d7e56f655cd8b9ff5107613ad4b7a332f1beb69
DimitrivanManen <17382@ma-web.nl>
2018-09-19 09:18:08 +0200

    Added new sprites and gravity update

---
+++ GPR Infinite Runner 17-9-2018/Assets/Scripts/GravityCharacter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityCharacter : MonoBehaviour {
+
+    Rigidbody2D player;
+    bool nagativeGravity = false;
+    public float gravityPower = 1;
+    public int rotateSpeed = 0;
+
+    private void Awake()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        Controls();
+    }
+    void Controls()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(nagativeGravity == false)
+            {
+                player.gravityScale = -gravityPower;
+                transform.up = Vector3.Lerp(transform.up, 12, rotateSpeed * Time.deltaTime);
+                nagativeGravity = true;
+            }
+            else
+            {
+                nagativeGravity = false;
+                player.gravityScale = gravityPower;
+            }
+        }
+    }
+}
