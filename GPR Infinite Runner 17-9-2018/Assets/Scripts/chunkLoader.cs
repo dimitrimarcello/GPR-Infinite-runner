caba9114dec92a3a965063edfd8528227dfb76f2
DimitrivanManen <17382@ma-web.nl>
2018-10-01 12:02:01 +0200

    Player fix starter chunk script

---
+++ GPR Infinite Runner 17-9-2018/Assets/Scripts/chunkLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chunkLoader : MonoBehaviour {
+
+    Transform _player;
+    [SerializeField]
+    Transform spawnLocation;
+    GameObject[] chunksEasy;
+    GameObject[] chunksMedium;
+    GameObject[] chunksHard;
+
+    private void Awake()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        chunksEasy = GameObject.FindGameObjectsWithTag("ChunksEasy");
+        chunksMedium = GameObject.FindGameObjectsWithTag("ChunksMedium");
+        chunksHard = GameObject.FindGameObjectsWithTag("ChunksHard");
+    }
+    private void Update()
+    {
+        
+    }
+    void LoadChunk()
+    {
+        if(transform.position.x >= _player.position.x)
+        {
+            
+            //Instantiate(chunksEasy[1],);
+        }
+    }
+
+}
