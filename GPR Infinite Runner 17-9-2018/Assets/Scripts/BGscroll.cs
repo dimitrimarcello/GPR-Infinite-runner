fc83bc2fba5ea4f1f477f3f15702d20b6eccc82e
jochemlam <24549@ma-web.nl>
2018-10-11 11:56:07 +0200

    finally fixed parallax

--- GPR Infinite Runner 17-9-2018/Assets/Scripts/BGscroll.cs
+++ GPR Infinite Runner 17-9-2018/Assets/Scripts/BGscroll.cs
@@ -9,16 +9,17 @@ public class BGscroll : MonoBehaviour
     private Transform camera;
 
     private Vector3 startPosition;
-
     void Start()
     {
         camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
-        startPosition = new Vector3(camera.position.x - 2, startPosition.y, startPosition.z);
+        
     }
 
     void Update()
     {
+        startPosition = new Vector3(camera.position.x - 24, transform.position.y, transform.position.z);
         newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeX);
-        transform.position = startPosition + new Vector3(1, 0, startPosition.z) * newPosition;
+        transform.position = startPosition + new Vector3(1, 0, 0) * newPosition;
+        
     }
 }
